f39ad8c3b83c8be6f03d86f807cb1051cef90f68
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-02 00:16:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
    
  if update_hash[key]
-  hash[key] += 1
+  update_hash[key] += 1
 else
   update_hash[key] = 1
 end
