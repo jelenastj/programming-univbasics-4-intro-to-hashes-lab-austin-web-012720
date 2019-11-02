46de621ed59c5886dfc6a22ad65c5edb6ad1b190
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-02 00:11:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,9 +42,8 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
    
    hash[key]
-  if hash[!key]
-     :key => 1
+ if hash[key]
+  shipping_manifest[key] += 1
 else
-     hash[key] += 1
-  
+  shipping_manifest["top hat"] = 1
 end
